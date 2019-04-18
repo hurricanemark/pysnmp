cebd954b136552f90f03f9bdf967749546cfc3f5
Mark Nguyen <markn@aesclever.com>
2019-04-18 08:13:32 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## Cisco, Networking, OSI, Programming, Python, QuickSNMP, Simple Network Management Protocol (SNMP), SNMP v1, SNMP v2c, SNMP v3 ##
+
+This code interfaces with the PySNMP module to retrieve MIBs data from an SNMP agent.  
+It prompts for input of HostIP address and community name.
+
+### Getting comfortable
+
+mkdir ~/py-snmp
+
+cd ~/py-snmp
+
+pip install --user pipenv
+
+pipenv install --python 3.7
+
+pipenv shell
+
+### Clone from GiHub
+
+
+### Run the program
+
+python3 -m test_quicksnmp [host_ip] [community_name]
+
+
+
+** Feel free to give it a star.
---
+++ snmpgetter.py
@@ -0,0 +1,79 @@
+from pysnmp import hlapi
+
+hlapi.CommunityData('public')
+
+def construct_object_types(list_of_oids):
+    object_types = []
+    for oid in list_of_oids:
+        object_types.append(hlapi.ObjectType(hlapi.ObjectIdentity(oid)))
+    return object_types 
+
+
+def fetch(handler, count):
+    result = []
+    for i in range(count):
+        try:
+            error_indication, error_status, error_index, var_binds = next(handler)
+            if not error_indication and not error_status:
+                items = {}
+                for var_bind in var_binds:
+                    items[str(var_bind[0])] = cast(var_bind[1])
+                result.append(items)
+            else:
+                raise RuntimeError('Got SNMP error: {0}'.format(error_indication))
+        except StopIteration:
+            break
+    return result
+
+def cast(value):
+    try:
+        return int(value)
+    except (ValueError, TypeError):
+        try:
+            return float(value)
+        except (ValueError, TypeError):
+            try:
+                return str(value)
+            except (ValueError, TypeError):
+                pass
+    return value
+
+
+def get(target, oids, credentials, port=161, engine=hlapi.SnmpEngine(), context=hlapi.ContextData()):
+    handler = hlapi.getCmd(
+        engine,
+        credentials,
+        hlapi.UdpTransportTarget((target, port)),
+        context,
+        *construct_object_types(oids)
+    )
+    return fetch(handler, 1)[0]
+
+
+def get_bulk(target, oids, credentials, count, start_from=0, port=161,
+             engine=hlapi.SnmpEngine(), context=hlapi.ContextData()):
+    handler = hlapi.bulkCmd(
+        engine,
+        credentials,
+        hlapi.UdpTransportTarget((target, port)),
+        context,
+        start_from, count,
+        *construct_object_types(oids)
+    )
+    return fetch(handler, count)
+
+def get_bulk_auto(target, oids, credentials, count_oid, start_from=0, port=161,
+                  engine=hlapi.SnmpEngine(), context=hlapi.ContextData()):
+    count = get(target, [count_oid], credentials, port, engine, context)[count_oid]
+    return get_bulk(target, oids, credentials, count, start_from, port, engine, context)
+
+
+
+if __name__ == '__main__':
+    print(get('localhost', ['1.3.6.1.2.1.1.5.0'], hlapi.CommunityData('public')))
+
+    its = get_bulk_auto('192.168.0.24', ['1.3.6.1.2.1.2.2.1.2 ', '1.3.6.1.2.1.31.1.1.1.18'], hlapi.CommunityData('public'), '1.3.6.1.2.1.2.1.0')
+    for it in its:
+        for k, v in it.items():
+            print("{0}={1}".format(k, v))
+        print('')
